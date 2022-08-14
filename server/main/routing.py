3db950b237db75850b3b22fcf62c89cce38d26c6
tanimsk <sktanim5800@gmail.com>
2022-08-14 23:13:21 +0600

    client

--- server/main/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import consumers
-
-ws_urlpatterns = [
-    path('ws/<str:uuid>/', consumers.NewConsumer.as_asgi()),
-]
--- server/server/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from main import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('documentation', views.documentation, name='doc'),
-    path('admin/', admin.site.urls),
-]
